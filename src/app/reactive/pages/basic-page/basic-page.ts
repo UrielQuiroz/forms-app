b0d66c1df5fef5e7d9db30125901ef364aa85877
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-26 15:11:41 -0600

    Mejorar re-utilización y experiencia de usuario

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -24,7 +24,7 @@ export class BasicPageComponent {
   // })
 
   isValidFiels(fieldName: string): boolean | null {
-    return !!this.myForm.controls[fieldName].errors
+    return (this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched)
   }
 
   getFieldError(fieldName: string): string | null {
@@ -46,5 +46,18 @@ export class BasicPageComponent {
     }
 
     return null;
+  }
+
+  onSave() {
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0
+    })
   }
 }
