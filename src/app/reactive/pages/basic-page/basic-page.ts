cc327198d642d24a73c3e48808ee3061739d1a1f
Uriel Quiroz <uriel.quiroz187@gmail.com>
2025-12-26 13:29:38 -0600

    ReactiveForms - FormGroup y FormControl

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,9 +1,16 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [ JsonPipe ],
+  imports: [ JsonPipe, ReactiveFormsModule ],
   templateUrl: './basic-page.html'
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+  myForm = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0)
+  })
+}
